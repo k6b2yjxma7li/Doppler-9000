8e52022f8c0b695ebc83d2e7331de09cdfff80a5
Robert <rkrasala@gmail.com>
2017-05-03 00:56:53 +0200

    Beginning of window class

--- Pojava/src/doppeler9k/Window.java
+++ Pojava/src/doppeler9k/Window.java
@@ -4,12 +4,20 @@ import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class Window extends JFrame {
 
 	public Window() throws HeadlessException {
 		JPanel mainPanel = new JPanel();
+		JMenuBar mainBar = new JMenuBar();
+		JMenu mainMenu = new JMenu("Menu");
+		JMenu function = new JMenu("Function");
+		
+		JMenuItem exitButton = new JMenuItem("Exit");
 		
 	}
 
