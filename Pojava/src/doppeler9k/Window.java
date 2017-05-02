72fad9bf69eb60e4f764ad606d7fd1f01f3cdfe5
rassch <274426@pw.edu.pl>
2017-05-02 14:34:29 +0200

    Zalążek nowej klasy okno.

---
+++ Pojava/src/doppeler9k/Window.java
@@ -0,0 +1,29 @@
+package doppeler9k;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+
+	public Window() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Window(GraphicsConfiguration arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Window(String arg0) throws HeadlessException {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Window(String arg0, GraphicsConfiguration arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
